421a11c9f3917c3d427942d4c2d7255ff664c61e
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-05-20 12:25:57 -0500

    Turned drag me into a button, made randomDragName a local variable so a new name generates every time the button is clicked

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+// name options
 var firstNames = ["Jackie", "Harmony", "Paris", "Londyn", "Nina"];
 var lastNames = ["Moody", "Ludicrous", "Fancie", "Poise", "Icy"];
 
+// function to return a random name 
 function randomize(array) {
-return array[Math.floor(Math.random(array) * array.length)];
+    return array[Math.floor(Math.random(array) * array.length)];
 }
 
-var randomDragName = randomize(firstNames) + " " + randomize(lastNames);
-
+// display the names on the page
 document.getElementById("dragNameButton").addEventListener("click", dragName);
 
-function dragName() {
-    document.getElementById("dragNameText").innerHTML = randomDragName;
-}
+    function dragName() {
+        // add the random names together
+        var randomDragName = randomize(firstNames) + " " + randomize(lastNames);
+        document.getElementById("dragNameText").innerHTML = randomDragName;
+    }
