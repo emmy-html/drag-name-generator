7828d675f1b319ad86a7ca34f1d09704fe1e314d
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-05-23 16:36:42 -0500

    Added the option to have a middle name & basic HTML formatting

--- script.js
+++ script.js
@@ -3,17 +3,31 @@ var firstNames = ["Jackie", "Harmony", "Paris", "Londyn", "Nina"];
 var lastNames = ["Moody", "Ludicrous", "Fancie", "Poise", "Icy"];
 var middleNames = ["Violet", "Magenta", "Blu", "Tangerine", "Honey"];
 
-// function to return a random name 
+// function to return a random name
 function randomize(array) {
-    return array[Math.floor(Math.random(array) * array.length)];
+  return array[Math.floor(Math.random(array) * array.length)];
 }
 
-// display the names on the page
+// assign middle name checkbox to a variable
+var middle = document.getElementById("middleNameOption");
+
+// display the name on the page when button is clicked
 document.getElementById("dragNameButton").addEventListener("click", dragName);
 
-    function dragName() {
-        // add the random names together
-        var randomDragName = randomize(firstNames) + " " + randomize(lastNames);
-        document.getElementById("dragNameText").innerHTML = randomDragName;
-        document.getElementById("dragNameButton").innerHTML = "Drag Me Again!";
-    }
+// function that makes all the drag magic happen!
+function dragName() {
+  var randomDragName = randomize(firstNames) + " " + randomize(lastNames);
+  var randomDragNameMiddle =
+    randomize(firstNames) +
+    " " +
+    randomize(middleNames) +
+    " " +
+    randomize(lastNames);
+  if (middle.checked === true) {
+    document.getElementById("dragNameText").innerHTML = randomDragNameMiddle;
+  } else {
+    document.getElementById("dragNameText").innerHTML = randomDragName;
+  }
+  document.getElementById("dragNameButton").innerHTML = "Drag Me Again!";
+  document.getElementById("condragulations").innerHTML = "Condragulations! Your new name is...";
+}
